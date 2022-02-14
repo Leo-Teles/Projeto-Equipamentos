3dfc585a7c8e8e0950d09f11b8b70535e22687a0
Leonardo Teles De Souza Menezes <46881551827@info.scs>
2022-02-14 11:22:49 -0300

    adição de coisas

--- 2022_3T_ProjetoBase_Backend/Patrimonio/Repositories/UsuarioRepository.cs
+++ 2022_3T_ProjetoBase_Backend/Patrimonio/Repositories/UsuarioRepository.cs
@@ -1,6 +1,8 @@
 ﻿using Patrimonio.Contexts;
+using Patrimonio.Controllers;
 using Patrimonio.Domains;
 using Patrimonio.Interfaces;
+using Patrimonio.Utils;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -20,7 +22,32 @@ namespace Patrimonio.Repositories
 
         public Usuario Login(string email, string senha)
         {
-            return ctx.Usuarios.FirstOrDefault(u => u.Email == email && u.Senha == senha);
+            var usuario = ctx.Usuarios.FirstOrDefault(u => u.Email == email);
+
+
+            if (usuario.Senha.Substring(0)!="$" && usuario.Senha.Substring(3) != "$")
+            {
+                var U = new UsuariosController(ctx);
+
+                usuario.Senha = Criptografis.GeraHash(usuario.Senha);
+
+                U.PutUsuario(usuario.Id, usuario);
+
+            }
+
+
+            if (usuario != null)
+            {
+                // Com o usuario encontrado temos a hash do banco para poder comparar com a senha vindo do formulário
+                bool comparado = Criptografis.Comparar(senha, usuario.Senha);
+                if (comparado)
+                    return usuario;
+            }
+
+
+
+
+            return null;
         }
     }
 }
---
+++ 2022_3T_ProjetoBase_Backend/Patrimonio/Utils/Criptografis.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Patrimonio.Utils
+{
+    public static class Criptografis
+    {
+
+        public static string GeraHash(string senha)
+        {
+            return BCrypt.Net.BCrypt.HashPassword(senha);
+        }
+
+        public static bool Comparar(string senhaLogin, string senhaBanco)
+        {
+            return BCrypt.Net.BCrypt.Verify(senhaLogin, senhaBanco);
+        }
+
+        
+    }
+}
